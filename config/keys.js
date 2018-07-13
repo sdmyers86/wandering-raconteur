9e1fa087924664e9edc42d84f7f20cb6f41719d9
Shawn <sdmyers86@yahoo.com>
2018-07-13 11:52:36 -0400

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://panda:panda123@ds137651.mlab.com:37651/wandering-dev',
+  googleClientID: '440739426860-gqbo2h586qo5o3sqhuuenmeknmp379rl.apps.googleusercontent.com',
+  googleClientSecret: 'mSzVAo-TZQobDt2WhH4LJfdy',
+}
